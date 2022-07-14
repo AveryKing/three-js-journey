bd9174892ebc15dc1212c1351604a1a6fb345d47
Avery King <kavery086@gmail.com>
2022-07-13 20:35:02 -0500

    camera added

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -8,4 +8,11 @@ const material = new THREE.MeshBasicMaterial({ color:0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+const sizes = {
+    width:800,
+    height:600
+}
+
 // Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+scene.add(camera)
