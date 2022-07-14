6f8d27fe58b96f23c8ec7e0cbd72e9a1030e0ab5
Avery King <kavery086@gmail.com>
2022-07-13 20:39:38 -0500

    renderer added

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,8 +1,9 @@
 
+
 // Scene
 const scene = new THREE.Scene()
 
-//Red cube
+// Red cube
 const geometry = new THREE.BoxGeometry(1,1,1)
 const material = new THREE.MeshBasicMaterial({ color:0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
@@ -16,3 +17,7 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({ canvas })
