978e037601ff7dd2acc55d5dbaa4a64694702124
Avery King <kavery086@gmail.com>
2022-07-13 20:22:10 -0500

    mesh added to scene

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,5 +1,11 @@
-/**
- * A scene is like a container. We put objects, models, lights, etc in it.
- */
 
+// Scene
 const scene = new THREE.Scene()
+
+//Red cube
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({ color:0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Camera
